046a669fb29267ed8644821828edd3c2e9986822
jasonhu <jasonhu1012@hotmail.com.tw>
2021-09-05 19:50:46 +0800

    generate images

--- pic_generator.py
+++ pic_generator.py
@@ -7,9 +7,9 @@ skip = 0
 for i in range(1100):
     try:
         Image.open(requests.get("https://picsum.photos/id/" + str(i) + "/1000", stream=True).raw).save("pic/" + str(i - skip) + ".jpg")
+        print(i - skip, "https://picsum.photos/id/" + str(i) + "/1000")
     except:
         skip += 1
-    print(i)
 print("skip:", skip)
 
 #unsplash
@@ -19,16 +19,19 @@ while True:
     url = f.readline()[:-1]
     if not url:
         break
-    img = Image.open(requests.get(url, stream=True).raw)
-    width = img.size[0]
-    height = img.size[1]
-    if width > height:
-        d = (width - height) // 2
-        box = (d, 0, width - d, height)
-    else:
-        d = (height - width) // 2
-        box = (0, d, width, height - d)
-    img.crop(box).resize((1000, 1000)).save("pic/" + str(count) + ".jpg")
-    print(count)
-    count += 1
+    try:
+        img = Image.open(requests.get(url, stream=True).raw)
+        width = img.size[0]
+        height = img.size[1]
+        if width > height:
+            d = (width - height) // 2
+            box = (d, 0, width - d, height)
+        else:
+            d = (height - width) // 2
+            box = (0, d, width, height - d)
+        img.crop(box).resize((1000, 1000)).save("pic/" + str(count) + ".jpg")
+        print(count, url)
+        count += 1
+    except:
+        continue
 f.close()
--- temp.py
+++ temp.py
@@ -8,16 +8,19 @@ while True:
     url = f.readline()[:-1]
     if not url:
         break
-    img = Image.open(requests.get(url, stream=True).raw)
-    width = img.size[0]
-    height = img.size[1]
-    if width > height:
-        d = (width - height) // 2
-        box = (d, 0, width - d, height)
-    else:
-        d = (height - width) // 2
-        box = (0, d, width, height - d)
-    img.crop(box).resize((1000, 1000)).save("pic/" + str(count) + ".jpg")
-    print(count)
-    count += 1
+    try:
+        img = Image.open(requests.get(url, stream=True).raw)
+        width = img.size[0]
+        height = img.size[1]
+        if width > height:
+            d = (width - height) // 2
+            box = (d, 0, width - d, height)
+        else:
+            d = (height - width) // 2
+            box = (0, d, width, height - d)
+        img.crop(box).resize((1000, 1000)).save("pic/" + str(count) + ".jpg")
+        print(count, url)
+        count += 1
+    except:
+        continue
 f.close()
