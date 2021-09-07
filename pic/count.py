6b0b9c3e255796b92e353a98ca994afb9bd71c04
jasonhu <jasonhu1012@hotmail.com.tw>
2021-09-07 19:36:07 +0800

    add a python program to count images

---
+++ pic/count.py
@@ -0,0 +1,14 @@
+import os
+
+allfile = os.listdir("./")
+allfile.sort()
+output = ""
+skip = ["blank.jpg", "cut.py", "duplicate.py", "count.py", "temp", ".DS_Store"]
+for file in allfile:
+    if file in skip:
+        continue
+    c = int(os.popen("ls " + file + " | wc -l").read())
+    if output:
+        output += ", "
+    output += file + ": " + str(c)
+print(output)
